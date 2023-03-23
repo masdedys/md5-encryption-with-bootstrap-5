062d4e6c175ee95f2779521109285021b4cabad2
masdedys <kakashi.setiawan@gmail.com>
2023-03-23 09:35:36 +0700

    ubah jadi via cdn aja

--- index.php
+++ index.php
@@ -1,45 +1,57 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-  <title>Enkripsi md5 - by mas_dedys</title>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="refresh" content="30">
+    <title>md-5 encryption by mas_dedys</title>
+    <link rel="shortcut icon" href="img/logo.png" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <!-- Bootstrap icon -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
 </head>
-
-<body>
-  <h1 align="center">Enkripsi md5</h1>
-  <form method="POST">
-    <div class="mb-3">
-      <label class="form-label">Tulis Text Asli:</label>
-      <input type="text" class="form-control" name="pass">
-    </div>
-    <div class="d-grid gap-2 col-6 mx-auto mb-3 row">
-      <input type="submit" name="send" class="btn btn-primary" value="kirim">
-    </div>
-
-    <?php
-    if (isset($_POST['send'])) {
-      $pass = $_POST['pass'];
-      $text = md5($_POST['pass']);
-      echo "
-	  <div class=\"mb-3\">
-      <label class=\"form-label\">Text asli anda:</label>
-      	<input type=\"text\" class=\"form-control\" value=\"$pass\" disabled>
-  	  </div>
-	  <div class=\"mb-3\">
-      <label class=\"form-label\">Tersandi sebagai:</label>
-      	<input type=\"text\" class=\"form-control\" value=\"$text\" disabled>
-  	  </div>
-  ";
-    }
-    ?>
-  </form><br>
-  
-  <script src="assets/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/js/bootstrap.min.js"></script>
-  <script src="assets/js/popper.min.js"></script>
-</body>
-
+  <body>
+    <section class="container content position-absolute top-50 start-50 translate-middle">
+        <div class="row text-center">
+            <div class="col-md">
+                <h2>MD5 Encryption</h2>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <form action="" method="post">
+                        <label for="text" class="mb-3">Tulis text yang akan disandikan:</label><br>
+                        <input type="text" class="form-control mb-3" id="text" placeholder="Masukkan Text" name="text_asli">
+                        <button type="submit" name="send" class="btn btn-primary mb-3"><i class="bi bi-key-fill"></i> Sandikan</button>
+                    
+                        <?php
+                            if (isset($_POST['send'])) {
+                            $text_asli = $_POST['text_asli'];
+                            $text_eknripsi = md5($_POST['text_asli']);
+                            echo "
+                            <div class=\"mb-3\">
+                            <label class=\"form-label\">Text asli anda:</label>
+                                <input type=\"text\" class=\"form-control\" value=\"$text_asli\" disabled>
+                            </div>
+                            <div class=\"mb-3\">
+                            <label class=\"form-label\">Tersandi sebagai:</label>
+                                <input type=\"text\" class=\"form-control\" value=\"$text_eknripsi\" disabled>
+                            </div>";
+                            }
+                        ?>
+                </form>
+            </div>
+        </div>
+    </section>
+    <section class="credit">
+      <div class="container-fluid fixed-bottom p-4" style="background-color: #e2edff;">
+        <div class="row">
+          <div class="col-md">
+            <p class="text-center">Created with <i data-feather="coffee"></i> and <i data-feather="heart" style="color: red;"> </i><a href="https://masdedys.github.io/"> by mas_dedys</a>. | &copy; 2023</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+  </body>
 </html>
